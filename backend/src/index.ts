a9102babb9a0fd507cc34a385dac39484bbb9c33
kavita <kavitasoren2000@gmail.com>
2025-11-01 00:56:34 +0530

    edit index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://primetradee.netlify.app",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
